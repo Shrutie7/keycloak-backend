c4ecd27cd26f2088e27a6d27b4f7fae882800097
Steak <lakhotiya.sil07@gmail.com>
2024-01-11 18:17:42 +0530

    created

---
+++ keycloak/src/main/java/com/testing/keycloak/controllers/CaptchaController.java
@@ -0,0 +1,31 @@
+package com.testing.keycloak.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testing.keycloak.services.CaptchaService;
+
+@RestController
+@RequestMapping("/captcha")
+public class CaptchaController {
+	
+	@Autowired
+	private CaptchaService captchaService;
+	
+	@GetMapping("/get")
+	private ResponseEntity<Map<String, Object>> getCaptcha(){
+		return captchaService.getCaptcha();
+	}
+	
+	@PostMapping("/validate")
+	private ResponseEntity<Map<String,Object>> validateCaptcha(@RequestBody String captchaData){
+		return captchaService.validateCaptcha(captchaData);
+	}
+}
---
+++ keycloak/src/main/java/com/testing/keycloak/services/CaptchaService.java
@@ -0,0 +1,13 @@
+package com.testing.keycloak.services;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+public interface CaptchaService {
+
+	public ResponseEntity<Map<String, Object>> getCaptcha();
+
+	public ResponseEntity<Map<String, Object>> validateCaptcha(String captchaData);
+
+}
